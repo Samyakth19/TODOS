b93592ff21fe278aeb2bd47ab7e511cb30a765fe
SAMYAKTH KUMAR <samyakthkumar19@gmail.com>
2022-12-06 23:54:36 +0530

    added a get method

--- todo_drf/api/views.py
+++ todo_drf/api/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 # Create your views here.
+
 @api_view(['GET'])
 def apiOverView(request):
     api_urls ={
@@ -13,3 +16,8 @@ def apiOverView(request):
         'Delete':'/task-delete/<str:pk>'
     }
     return Response(api_urls)
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializers = TaskSerializer(tasks, many=True)
+    return Response(serializers.data)
