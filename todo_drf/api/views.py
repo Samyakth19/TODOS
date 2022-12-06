c0a0f82bb9d2ca0087f342a09f6dd80b5e9a7c6d
SAMYAKTH KUMAR <samyakthkumar19@gmail.com>
2022-12-07 00:09:37 +0530

    intial commit

--- todo_drf/api/views.py
+++ todo_drf/api/views.py
@@ -12,4 +12,14 @@ def apiOverView(request):
         'Update':'/task-update/<str:pk>',
         'Delete':'/task-delete/<str:pk>'
     }
+<<<<<<< Updated upstream
     return Response(api_urls)
+=======
+    return Response(api_urls)
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializers = TaskSerializer(tasks, many=True)
+    return Response(serializers.data)
+
+>>>>>>> Stashed changes
