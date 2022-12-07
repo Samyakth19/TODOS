da37ddaefdd316147eacfbe4a9bcff819525d096
SAMYAKTH KUMAR <samyakthkumar19@gmail.com>
2022-12-07 20:47:22 +0530

    final commit

--- todo_drf/api/views.py
+++ todo_drf/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -54,4 +55,5 @@ def taskDelete(request,pk):
     serializers = TaskSerializer(instance=task,data=request.data)
     task.delete()
     return Response(serializers.data)
+
 
